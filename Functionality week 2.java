0febcd2f3e5441375190dda77718f5eb57af5323
Alexander L. Wolf <109392016+AlexanderLucasWolf@users.noreply.github.com>
2022-11-25 12:20:17 +0100

    Add files via upload

---
+++ Functionality week 2.java
@@ -0,0 +1,59 @@
+
+public class Functionality {
+
+	public static String longestWord(String a) {
+		String longestWord = "";
+		String currentWord = "";
+		char currentLetter;
+		if (a == "") {
+			return "";
+		}
+		for (int i = 0; i < a.length(); i++) {
+			currentLetter = a.charAt(i);
+			if (currentLetter != ' ') {
+				currentWord += a.charAt(i);
+			} else {
+				if (currentWord.length() > longestWord.length()) {
+					longestWord = currentWord;
+				}
+				currentWord = "";
+			}
+			if (i == a.length() - 1) {
+				if (currentWord.length() > longestWord.length()) {
+					longestWord = currentWord;
+				}
+			}
+		}
+		longestWord = longestWord + " " + longestWord.length();
+		return longestWord;
+	}
+
+	public static int[][] decompress(int arr[]) {
+		int laenge1;
+		int laenge2;
+		if (arr.length % 2 == 0) {
+			laenge1 = arr.length / 2;
+			laenge2 = arr.length / 2;
+		} else {
+			laenge1 = (arr.length + 1) / 2;
+			laenge2 = (arr.length - 1) / 2;
+		}
+
+		int[][] newArr = new int[2][];
+		newArr[0] = new int[laenge1];
+		newArr[1] = new int[laenge2];
+
+		for (int i = 0; i < laenge1; i++) {
+			newArr[0][i] = arr[i];
+		}
+		for (int i = 0; i < laenge2; i++) {
+			newArr[1][i] = arr[i + laenge1];
+		}
+		return newArr;
+	}
+
+	public static void main(String[] args) {
+		longestWord("Universitaet Koblenz-Landau");
+		decompress(new int[] { -2, -6, 10, 98, 7 });
+	}
+}
